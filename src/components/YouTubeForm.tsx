e7249fa89fa7b84b5dcda0efc32651f08f49e34d
kafui4k <kalordo7@gmail.com>
2024-03-11 11:00:11 +0000

    implement async validation using the emailAvailable option on validate

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -164,6 +164,13 @@ export default function YouTubeForm() {
                     "This domain is not supported"
                   );
                 },
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length == 0 || "Email already exists.";
+                },
               },
             })}
           />
